e2dd5ed922a95ff4a4b4eeeec58c7522dfedf4fd
Nathan Feldsien <n.feldsien@me.com>
2018-01-25 11:32:48 -0500

    Switches .then() statements to node async/await calls, requires node 7.7.6+

--- test/TestGladiusToken.js
+++ test/TestGladiusToken.js
@@ -10,77 +10,53 @@ contract('GladiusToken', function(accounts) {
 
   let initialSupply = 10000 * (10**8)
 
-  function getInstance() {
-    return GladiusToken.deployed().then(function(instance) {
-      return instance
-    })
-  }
-
   // Test creation of tokens
-  it('Deploy Tokens to Creator\'s Account', function() {
-    // Create initial supply of tokens
-    getInstance().then(function(instance) {
-      return instance.balanceOf.call(creator)
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), initialSupply, 'Initial tokens were not allocated to the creator\'s account')
-    })  
+  it('Deploy Tokens to Creator\'s Account', async function() {
+    let gladius = await GladiusToken.deployed()
+    let balance = await gladius.balanceOf.call(accounts[0])
+    assert.equal(balance.valueOf(), initialSupply, 'Initial tokens were not allocated to the creator\'s account')
   })
 
   // Test balance transfers
-  it('Simulate transfer from creator to user', function() {
-    let self
-    
-    // Balance ladger
-    let creatorStartBalance
-    let userStartBalance
-    let creatorEndBalance
-    let userEndBalance
-
+  it('Simulate transfer from creator to user', async function() {
     let transferAmount = 10
     
     // Grab instance
-    getInstance().then(function(instance) {
-      self = instance
-      return self.balanceOf.call(creator) 
-    }).then(function(creatorBalance) {
-      creatorStartBalance = creatorBalance.toNumber()     // Save creator's starting balance
-      return self.balanceOf.call(user)
-    }).then(function(userBalance) {
-      userStartBalance = userBalance.toNumber()           // Save user's starting balance
-      return self.transfer(user, transferAmount)          // Initiate Transfer
-    }).then(function() {
-      return self.balanceOf.call(creator) 
-    }).then(function(creatorBalance) {
-      creatorEndBalance = creatorBalance.toNumber()       // Save creator's ending balance
-      return self.balanceOf.call(user)
-    }).then(function(userBalance) {
-      userEndBalance = userBalance.toNumber()             // Save creator's ending balance
+    let gladius = await GladiusToken.deployed()
+    let creatorStartBalance = await gladius.balanceOf.call(creator)
+    let userStartBalance = await gladius.balanceOf.call(user)
 
-      // Compare transfers to ledger math
-      assert.equal(creatorEndBalance, creatorStartBalance - transferAmount, 'Amount wasn\'t correctly taken from the sender')
-      assert.equal(userEndBalance, userStartBalance + transferAmount, 'Amount wasn\'t correctly sent to the receiver')
-    })
+    await gladius.transfer(user, transferAmount)
+
+    let creatorEndBalance = await gladius.balanceOf.call(creator)
+    let userEndBalance = await gladius.balanceOf.call(user)
+
+    assert.equal(
+      creatorEndBalance.toNumber(),
+      creatorStartBalance.toNumber() - transferAmount,
+      'Amount wasn\'t correctly taken from the sender'
+    )
+    assert.equal(
+      userEndBalance.toNumber(),
+      userStartBalance.toNumber() + transferAmount,
+      'Amount wasn\'t correctly sent to the receiver'
+    )
   })
 
-  it('Simulate burning tokens', function() {
-    let self
-    let burnAmount = 50
-    let startBalance
+  it('Simulate burning tokens', async function() {
+    let burnAmount = 50.0
     
-    getInstance().then(function(instance) {
-      self = instance
-      return self.totalSupply.call()
-    }).then(function(total) {
-      startBalance = total.toNumber()
-      return self.burn(burnAmount)
-    }).then(function() {
-      return self.totalSupply.call()
-    }).then(function(endTotal) {
-      console.log(startBalance)
-      console.log(burnAmount)
-      let endingSupply = endTotal.valueOf()
-      console.log(endingSupply)
-      assert.equal(endingSupply, startBalance - burnAmount, 'Tokens did not successfully burn')
-    })
+    let gladius = await GladiusToken.deployed()
+    let totalInitialSupply = await gladius.totalSupply.call()
+
+    await gladius.burn(burnAmount)
+
+    let totalEndingSupply = await gladius.totalSupply.call()
+
+    assert.equal(
+      totalEndingSupply.toNumber(),
+      totalInitialSupply.toNumber() - burnAmount,
+      'Tokens did not successfully burn'
+    )
   })
 })
