f581e1227b404f6a0e71855a2e9d21b7399e7ef9
Nathan Feldsien <n.feldsien@me.com>
2018-01-26 10:26:46 -0500

    Removes semicolon

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -5,5 +5,5 @@ let GladiusToken = artifacts.require('GladiusToken')
 module.exports = function(deployer) {
   deployer.deploy(GladiusToken, 10000, 'Gladius Token', 'GLA').then( ()=> {
     return deployer.deploy(MarketContract, GladiusToken.address, 10, 10)
-  });
+  })
 }
