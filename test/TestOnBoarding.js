247e0eaccc909735aff11dc28a96676923cb9f12
Nathan Feldsien <nate@studioember.com>
2018-03-06 16:47:43 -0500

    Adds jshint ignore and onboarding test stubs

---
+++ test/TestOnBoarding.js
@@ -0,0 +1,42 @@
+
+
+contract('Onboarding', function() {
+  let signUpNode = accounts[6]
+
+  it('Signup Node', async function() { // jshint ignore:line
+    // Pseudo Code Node
+    //
+    // Get account from meta-mask
+    // Ask for private and public key
+    // Ask for account details
+    // Encrypt and submit node request
+
+  }) // jshint ignore:line
+  
+  it('View Node\'s Details', async function() { // jshint ignore:line
+    // Pseudo Code Node Details
+    //
+    // Read private key
+    // Decrypt payload
+    // Display details
+
+  }) // jshint ignore:line
+  
+  it('Request access to Pool', async function() { // jshint ignore:line
+    // Pseudo Code Pool Approve Node
+    //
+    // Get Node Address
+    // Decrypt Node details
+    // Approve / Deny request
+    // Update Node status
+    // Add Node to approved list
+
+  }) // jshint ignore:line
+
+  it('Read Node\'s Status', async function() { // jshint ignore:line
+    // Pseudo Code Node Status
+    //
+    // Get Node Address
+    // Display public details / status
+  }) // jshint ignore:line
+})
