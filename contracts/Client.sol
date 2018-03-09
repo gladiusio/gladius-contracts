25c51dd2df3516d8cf0789253b059e4b37a886ab
Nathan Feldsien <nate@studioember.com>
2018-03-09 16:41:13 -0500

    Adds getPoolData to Client

--- contracts/Client.sol
+++ contracts/Client.sol
@@ -23,7 +23,11 @@ contract Client {
     return status[_pool];
   }
 
-  function getData(address _pool) public view returns(string){
+  function getData() public view returns(string){
+    return data;
+  }
+
+  function getPoolData(address _pool) public view returns(string){
     return poolData[_pool];
   }
 
