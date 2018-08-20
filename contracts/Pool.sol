3cb2a81c049da201ad6836a56e68f90304dd3d66
Marcelo McAndrew <marcelomcandrew@gmail.com>
2018-08-20 16:30:52 -0400

    trimmed the fat

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,234 +1,73 @@
 pragma solidity ^0.4.19;
 
-import "./Node.sol";
-import "./Client.sol";
-import "./AbstractBalance.sol";
 //ALL DATA VARIABLES ARE SUBJECT TO CHANGE (STC)
 
-contract Pool is AbstractBalance {
-  string public publicKey;                           //a public RSA key to encrypt against
+contract Pool {
 
-  address private owner;                             //msg.sender = marketplace; therefore we need to pass in an owner manually
-  address[] public nodeOwners;
-  address[] public clientOwners;
-  address[] private client_list;                     //list of client proposals
-  address[] private node_list;                       //list of node proposals
+  address public owner;
+  address[] public nodes;
+  address[] public clients;
+  address[] public masterNodes;
 
-  mapping (address => Client) private clients;       //client requesting pool for protection/cdn
-  mapping (address => Node) private nodes;           //node information (proposal)
-  mapping (address => Balance) userBalance;          //maps a client's address to a balance struct
+  string public seedNode;
+  string public data;
+
 
   /**
    * Create new Pool and assign owner
    *
    * Data is assigned owner and uses the owner's public key
-   * @param _publicKey Owner's public RSA key to encrypt against
    * @param _owner Address of the owner
    */
-  constructor(string _publicKey, address _owner) public {
-    publicKey = _publicKey;
+  constructor(address _owner) public {
     owner = _owner;
   }
 
+  function getNodes() public view returns(address[]) {
+    return nodes;
+  }
+
+  function getClients() public view returns(address[]) {
+    return clients;
+  }
+
   /**
-   * Makes and returns the Balance struct for a given user
+   * Add a master node to the list of master nodes
    *
-   * @param _user User address to query userBalance array
-   * @return (uint,uint,uint,uint)
+   * @param _node Node to add to the list
    */
-  function getBalanceStructFor(address _user) public view returns (uint,uint,uint,uint) {
-    return (userBalance[_user].owed, userBalance[_user].total, userBalance[_user].completed, userBalance[_user].paid);
+  function addMasterNode(address _node) public {
+    require(msg.sender == owner);
+    masterNodes.push(_node);
   }
 
   /**
-   * Returns the total from a given users balance
+   * Add a node to the list of master nodes
    *
-   * @param _user User address to query userBalance[_user].total
-   * @return (uint)
+   * @param _node Node to add to the list
    */
-  function getTotalBalanceFor(address _user) public view returns (uint) {
-    return userBalance[_user].total;
+  function addMasterNode(address _node) public {
+    require(msg.sender == owner);
+    masterNodes.push(_node);
   }
 
   /**
-   * Returns the owed balance from a given users balance
+   * Add a node to the list of nodes
    *
-   * @param _user User address to query userBalance[_user].owed
-   * @return (uint)
+   * @param _node Node to add to the list
    */
-  function getOwedBalanceFor(address _user) public view returns (uint) {
-    userBalance[_user].owed;
+  function addNode(address _node) public {
+    require(msg.sender == owner);
+    nodes.push(_node);
   }
 
   /**
-   * Allocates a clients funds into the Pools balance and the users balance
+   * Add a client to the list of clients
    *
-   * @param _client address
-   * @param _amount uint
-   * @return bool TODO run through checks to ensure transfer logic passes
-   */
-  function allocateFundsFrom(address _client, uint _amount) public returns (bool) {
-    allocateFunds(_amount);
-
-    Balance storage _userBalance = userBalance[_client];
-
-    userBalance[_client] = Balance({
-      owed : _userBalance.owed,
-      total : _userBalance.total + _amount,
-      completed : _userBalance.completed,
-      paid : _userBalance.paid
-    });
-
-    return true;
-  }
-
-  /**
-   * Records an amount of work done by a node to the balance
-   *
-   * @param _node Node that completed the work
-   * @param _client Client that will be billed for the nodes work
-   * @param _amount Amount of work completed
-   */
-  function logWorkFrom(address _node, address _client, uint _amount) public returns (bool) {
-    Balance storage _nodeBalance = userBalance[_node];
-    Balance storage _clientBalance = userBalance[_client];
-
-    if (work(_amount) && _amount > _clientBalance.total) {
-      revert();
-    }
-
-    // Node Balance
-    userBalance[_node] = Balance({
-      owed : _nodeBalance.owed + _amount,
-      total : _nodeBalance.total,
-      completed : _nodeBalance.completed + _amount,
-      paid : _nodeBalance.paid
-    });
-
-    // Client Balance
-    userBalance[_client] = Balance({
-      owed : _clientBalance.owed,
-      total : _clientBalance.total - _amount,
-      completed : _clientBalance.completed + _amount,
-      paid : _clientBalance.paid
-    });
-
-    return true;
-  }
-
-  /**
-   * Recalculates the balances of the pool and user after a payout
-   *
-   * @param _node Node that completed the work
-   * @param _amount Amount of work completed
-   * @return bool true if withdraw was successful
-   */
-  function payout(address _node, uint _amount) public returns (bool) {
-    Balance storage _nodeBalance = userBalance[_node];
-
-    if (pay(_amount) && _amount > _nodeBalance.owed) {
-      revert();
-    }
-
-    userBalance[_node] = Balance({
-      owed : _nodeBalance.owed - _amount,
-      total : _nodeBalance.total,
-      completed : _nodeBalance.completed,
-      paid : _nodeBalance.paid + _amount
-    });
-
-    return true;
-  }
-
-  function getNodeList() public view returns(address[]) {
-    return node_list;
-  }
-
-  function getNodeOwnerList() public view returns(address[]) {
-    return nodeOwners;
-  }
-
-  function getClientOwnerList() public view returns(address[]) {
-    return clientOwners;
-  }
-
-  function getClientList() public view returns(address[]) {
-    return client_list;
-  }
-
-  /**
-   * Add a node to the list of nodes only if it's applied
-   * called by the node contract durring applyToPool
-   *
-   */
-  function addNode() external {
-    Node _newNode = Node(msg.sender);
-    require(_newNode.getStatus(address(this)) == 3); //make sure the node has applied to this pool
-
-    node_list.push(msg.sender);
-    nodeOwners.push(_newNode.getOwner());
-    nodes[msg.sender] = _newNode;
-  }
-
-  /**
-   * Add a client to the list of clients only if it's applied
-   * called by the client contract durring applyToPool
-   *
+   * @param _node Node to add to the list
    */
   function addClient() external {
-    Client _newClient = Client(msg.sender);
-    require(_newClient.getStatus(address(this)) == 3); //make sure the client has applied to this pool
-
-    client_list.push(msg.sender);
-    clientOwners.push(_newClient.getOwner());
-    clients[msg.sender] = _newClient;
-  }
-
-  /**
-   * Accept a node
-   *
-   * @param _node address of the applying node
-   */
-  function acceptNode(address _node) public {
     require(msg.sender == owner);
-    Node _applicant = Node(_node);
-    require(_applicant.getStatus(address(this)) == 3);
-    _applicant.setStatus(1);
-  }
-
-  /**
-   * Accept a client
-   *
-   * @param _client address of the applying node
-   */
-  function acceptClient(address _client) public {
-    require(msg.sender == owner);
-    Client _applicant = Client(_client);
-    require(_applicant.getStatus(address(this)) == 3);
-    _applicant.setStatus(1);
-  }
-
-  /**
-   * Remove a member
-   * Must be the owner
-   * @param _node address to be removed
-   */
-  function rejectNode(address _node) public {
-    require(msg.sender == owner);
-    Node _applicant = Node(_node);
-    require(_applicant.getStatus(address(this)) == 3);
-    _applicant.setStatus(2);
-  }
-
-  /**
-  * Remove a member
-  * Must be the owner
-  * @param _client address to be removed
-  */
-  function rejectClient(address _client) public {
-    require(msg.sender == owner);
-    Client _applicant = Client(_client);
-    require(_applicant.getStatus(address(this)) == 3);
-    _applicant.setStatus(2);
+    clients.push(client);
   }
 }
